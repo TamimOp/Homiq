80fc29cae0f8437c897fbdf0a21005add5904fee
TamimOp <tamimshadanik@gmail.com>
2025-06-29 19:56:54 +0600

    update FAQ component to correct image path for property value question

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -9,7 +9,7 @@ const faqs = [
     question: "Discover Properties with the Best Value",
     answer:
       "Estatein provided us with top-notch service. They helped us sell our property quickly and at a great price. We couldn't be happier with the results.",
-    image: "/assets/project1.png",
+    image: "/assets/project2.png",
   },
   {
     question: "What documents do I need to sell my property through Estatein?",
